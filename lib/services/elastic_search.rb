8371718415fae3a9a05e4efdb2afb21fce1ec1e8
Rachel Slaby <this@belongstorachel.com>
2016-02-16 15:48:18 -0500

    changed the interface a little so it's optional to wait

--- lib/services/elastic_search.rb
+++ lib/services/elastic_search.rb
@@ -4,13 +4,6 @@ class ElasticSearch
 
   @@client ||= Aws::ElasticsearchService::Client.new
 
-  def self.processing?(domain_name)
-    es_domain = self.retrieve(domain_name)
-    raise "Elasticsearch domain not found" unless es_domain
-
-    es_domain.domain_status.processing
-  end
-
   def self.retrieve(domain_name)
     es_domain = @@client.describe_elasticsearch_domain({
         domain_name: domain_name,
@@ -29,13 +22,6 @@ class ElasticSearch
 
     @@client.create_elasticsearch_domain(opts)
 
-    # wait until processing is completed?
-    # Endpoint won't be available until then
-    while self.processing?(domain_name)
-      puts "Elasticsearch domain is still processing..."
-      sleep 30
-    end
-
     puts "Elasticsearch domain created."
     self.retrieve(domain_name)
   end
@@ -48,6 +34,35 @@ class ElasticSearch
     else
       es_domain = self.create(domain_name, opts = {})
     end
+  end
+
+  def self.ensure_endpoint_available(domain_name)
+    # wait until processing is completed
+    # Endpoint won't be available until then
+    while self.processing?(domain_name) || !self.endpoint_available?(domain_name)
+      puts "Elasticsearch domain is still processing..."
+      sleep 30
+    end
+
+    self.endpoint(domain_name)
+  end
+
+  def self.processing?(domain_name)
+    es_domain = self.retrieve(domain_name)
+    raise "Elasticsearch domain not found" unless es_domain
+
+    es_domain.domain_status.processing
+  end
+
+  def self.endpoint_available?(domain_name)
+    !self.endpoint(domain_name).nil?
+  end
+
+  def self.endpoint(domain_name)
+    es_domain = self.retrieve(domain_name)
+    raise "Elasticsearch domain not found" unless es_domain
+
+    es_domain.domain_status.endpoint
   end
 
 end
