4c3164569184041e539492d68cb223a3a239e6ac
Rob Kinyon <rob.kinyon@gmail.com>
2016-06-08 17:43:03 -0400

    Get coverage up to 90+% for Cloudfront (do not know how to test waiters)

--- spec/cloudfront_spec.rb
+++ spec/cloudfront_spec.rb
@@ -82,6 +82,38 @@ describe Cloudfront do
     stub_calls(*stubs)
   end
 
+  def cd_response
+    {
+      distribution: {
+        id: 'abcd',
+        status: 'pending',
+        last_modified_time: Time.now,
+        in_progress_invalidation_batches: 0,
+        domain_name: 'abcd',
+        active_trusted_signers: {enabled: false, quantity:0},
+        distribution_config: {
+          caller_reference: 'abcd',
+          origins: {quantity:0},
+          default_cache_behavior: {
+            target_origin_id: 'abcd',
+            forwarded_values: {
+              query_string: true,
+              cookies: { forward: 'all' },
+            },
+            trusted_signers: {
+              enabled: true,
+              quantity: 0,
+            },
+            viewer_protocol_policy: 'allow-all',
+            min_ttl: 10,
+          },
+          comment: 'abcd',
+          enabled: true,
+        },
+      },
+    }
+  end
+
   context '#find_distribution_for_domain' do
     context 'when no distributions' do
       before { build_ld_stubs([]) }
@@ -113,8 +145,172 @@ describe Cloudfront do
   end
 
   context '#ensure_distribution_for_s3' do
+    context 'when the distribution exists' do
+      before { build_ld_stubs(['my-bucket.s3.amazonaws.com']) }
+
+      it 'finds the distribution' do
+        expect(Cloudfront.ensure_distribution_for_s3('my-bucket')).to be_truthy
+      end
+    end
+
+    context 'when the distribution does not exist' do
+      before { build_ld_stubs([]) }
+
+      it 'creates the distribution with default values' do
+        bucket_name = 'my-bucket'
+
+        stub_calls([:create_distribution, {
+          distribution_config: {
+            caller_reference: bucket_name,
+            aliases: {
+              quantity: 0,
+              items: [],
+            },
+            default_root_object: 'index.html',
+            origins: {
+              quantity: 1,
+              items: [
+                {
+                  id: bucket_name,
+                  domain_name: "#{bucket_name}.s3.amazonaws.com",
+                  s3_origin_config: { origin_access_identity: "" },
+                },
+              ],
+            },
+            default_cache_behavior: {
+              target_origin_id: bucket_name,
+              forwarded_values: {
+                query_string: false,
+                cookies: { forward: 'all' },
+              },
+              trusted_signers: {
+                enabled: false,
+                quantity: 0,
+              },
+              viewer_protocol_policy: 'allow-all',
+              min_ttl: 60,
+              max_ttl: 300,
+              default_ttl: 60,
+            },
+            cache_behaviors: { quantity: 0 },
+            comment: "Distribution for #{bucket_name}",
+            logging: {
+              enabled: false,
+              include_cookies: false,
+              bucket: '',
+              prefix: '',
+            },
+            viewer_certificate: {
+              cloud_front_default_certificate: false,
+              ssl_support_method: 'sni-only',
+              minimum_protocol_version: 'TLSv1',
+            },
+            enabled: true,
+          }
+        }, cd_response])
+
+        expect(Cloudfront.ensure_distribution_for_s3(bucket_name)).to be_truthy
+      end
+
+      it 'creates the distribution with SSL override' do
+        bucket_name = 'my-bucket'
+
+        stub_calls([:create_distribution, {
+          distribution_config: {
+            caller_reference: bucket_name,
+            aliases: {
+              quantity: 0,
+              items: [],
+            },
+            default_root_object: 'index.html',
+            origins: {
+              quantity: 1,
+              items: [
+                {
+                  id: bucket_name,
+                  domain_name: "#{bucket_name}.s3.amazonaws.com",
+                  s3_origin_config: { origin_access_identity: "" },
+                },
+              ],
+            },
+            default_cache_behavior: {
+              target_origin_id: bucket_name,
+              forwarded_values: {
+                query_string: false,
+                cookies: { forward: 'all' },
+              },
+              trusted_signers: {
+                enabled: false,
+                quantity: 0,
+              },
+              viewer_protocol_policy: 'redirect-to-https',
+              min_ttl: 60,
+              max_ttl: 300,
+              default_ttl: 60,
+            },
+            cache_behaviors: { quantity: 0 },
+            comment: "Distribution for #{bucket_name}",
+            logging: {
+              enabled: false,
+              include_cookies: false,
+              bucket: '',
+              prefix: '',
+            },
+            viewer_certificate: {
+              cloud_front_default_certificate: false,
+              ssl_support_method: 'sni-only',
+              minimum_protocol_version: 'TLSv1',
+              acm_certificate_arn: 'cert1',
+              certificate_source: 'acm',
+            },
+            enabled: true,
+          }
+        }, cd_response])
+
+        expect(
+          Cloudfront.ensure_distribution_for_s3(
+            bucket_name, acm_ssl: OpenStruct.new({certificate_arn: 'cert1'}),
+          )
+        ).to be_truthy
+      end
+    end
   end
 
-  context '#waitfor_distribution_deployed' do
+  # I don't understand how to stub out a waiter.
+  xcontext '#waitfor_distribution_deployed' do
+    it "returns true when successful" do
+      distro = OpenStruct.new({id: 'abcd'})
+      allow_any_instance_of(Kernel).to receive(:sleep)
+      stub_calls(
+        [:wait_until, [:distribution_deployed, id: distro.id], true],
+        #[:get_distribution, {}, nil],
+        [:get_distribution, {id: distro.id}, {distribution:{
+          id:'abcd',
+          status: 'Deployed',
+          last_modified_time: Time.now,
+          in_progress_invalidation_batches: 0,
+          domain_name: 'foo.com',
+          active_trusted_signers: {enabled: true, quantity: 0},
+          distribution_config: {
+            caller_reference: 'abcd',
+            origins: {quantity: 0},
+            default_cache_behavior: {
+              target_origin_id: 'some id',
+              forwarded_values: {
+                query_string: true, cookies: { forward: 'all' },
+              },
+              trusted_signers: { enabled: false, quantity: 0 },
+              viewer_protocol_policy: 'allow-all',
+              min_ttl: 10,
+            },
+            comment: 'some comment',
+            enabled: true,
+          },
+        }}],
+      )
+      expect {
+        expect(Cloudfront.waitfor_distribution_deployed(distro)).to be_truthy
+      }.to output(/Waiting for distribution to be deployed/).to_stdout
+    end
   end
 end
