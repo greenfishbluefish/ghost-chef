e4f87a2b2b1bff266313958aee2be586ad52b3b2
Rob Kinyon <rob.kinyon@gmail.com>
2016-06-13 09:54:45 -0400

    Descend multiple keys to find the values

--- lib/util.rb
+++ lib/util.rb
@@ -1,14 +1,23 @@
 class Util
+  def self.descend(obj, keys)
+    value = obj
+    keys.each { |k| value = value.send(k) }
+    return value
+  end
+
   def self.filter(obj, method, args, keys, continue, &filter)
     method = method.to_sym
     continue_check = continue[0].to_sym
     continue_value = continue[1].to_sym
 
+    keys = [keys] if !keys.is_a? Array
+    keys = keys.map { |k| k.to_sym }
+
     rv = obj.send(method, **args)
-    items = (rv.send(keys.to_sym) || []).select(&filter)
+    items = (self.descend(rv, keys) || []).select(&filter)
     while x = rv.send(continue_check)
       rv = obj.send(method, **args.merge(continue_value => x))
-      items.concat (rv.send(keys.to_sym) || []).select(&filter)
+      items.concat (self.descend(rv, keys) || []).select(&filter)
     end
 
     return items
--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -17,38 +17,44 @@ describe Util do
     # * #3 is a Hash or nil
     # * #4 is a String|Symbol, or an Array of String|Symbol (normalize to Array)
     # * #5 is an Array[String|Symbol] where .size == 2
-    # * The RV of #2 will respond to #4[0] (recursively) (1, 2, & 3 levels)
+    # * The RV of #2 will respond to #4[0] (recursively) (N levels)
+    #   * Descending will catch where #2 doesn't respond to #4
+    # * The RV from #2.#4 will be an Array or falsy
     # * #6 exists and is the signature (Scalar)->Bool
 
     # Test cases:
-    # * Method returns 0 (stop), filter -> 0
-    it "returns an empty array on nothing returned" do
-      obj = Class.new do
-        def method(opts={})
-          OpenStruct.new(key1: nil, check: nil)
-        end
-      end.new
-      expect(Util.filter(obj, :method, {}, :key1, [ :check, :next ]) { true }).to eql []
+    context "when 0 is returned" do
+      let (:obj) {
+        Class.new do
+          def method(opts={})
+            OpenStruct.new(key1: nil, check: nil)
+          end
+        end.new
+      }
+
+      it "returns no values when filter is closed" do
+        expect(Util.filter(obj, :method, {}, :key1, [ :check, :next ]) { false }).to eql []
+      end
+      it "returns no values when filter is open" do
+        expect(Util.filter(obj, :method, {}, :key1, [ :check, :next ]) { true }).to eql []
+      end
     end
 
-    # * Method returns 1 (stop), filter -> 0
-    it "returns one value when one is returned and filter is open" do
-      obj = Class.new do
-        def method(opts={})
-          OpenStruct.new(key1: [ 'a' ], check: nil)
-        end
-      end.new
-      expect(Util.filter(obj, :method, {}, :key1, [ :check, :next ]) { false }).to eql []
-    end
+    context "when 1 is returned" do
+      let (:obj) {
+        Class.new do
+          def method(opts={})
+            OpenStruct.new(key1: [ 'a' ], check: nil)
+          end
+        end.new
+      }
 
-    # * Method returns 1 (stop), filter -> 1
-    it "returns one value when one is returned and filter is open" do
-      obj = Class.new do
-        def method(opts={})
-          OpenStruct.new(key1: [ 'a' ], check: nil)
-        end
-      end.new
-      expect(Util.filter(obj, :method, {}, :key1, [ :check, :next ]) { true }).to eql ['a']
+      it "returns no values when filter is closed" do
+        expect(Util.filter(obj, :method, {}, :key1, [ :check, :next ]) { false }).to eql []
+      end
+      it "returns all values when filter is open" do
+        expect(Util.filter(obj, :method, {}, :key1, [ :check, :next ]) { true }).to eql ['a']
+      end
     end
 
     context "when 2+1 is returned" do
@@ -63,6 +69,7 @@ describe Util do
           end
         end.new
       }
+
       it "returns all values when filter is open" do
         expect(
           Util.filter(obj, :method, {}, :key1, [ :check, :next ]) { true }
@@ -75,7 +82,6 @@ describe Util do
       end
     end
 
-    # * Method returns 2 + 2 + 1 (stop), filter -> 0
     context "when 2+2+1 is returned" do
       let(:obj) {
         Class.new do
@@ -90,6 +96,7 @@ describe Util do
           end
         end.new
       }
+
       it "returns all values when filter is open" do
         expect(
           Util.filter(obj, :method, {}, :key1, [ :check, :next ]) { true }
@@ -102,6 +109,61 @@ describe Util do
       end
     end
 
-    # Multiple keys
+    context "when descending multiple keys" do
+      context "when 1 is returned, 2 keys" do
+        let (:obj) {
+          Class.new do
+            def method(opts={})
+              OpenStruct.new(key1: OpenStruct.new(key2: [ 'a' ]), check: nil)
+            end
+          end.new
+        }
+
+        it "returns no values when filter is closed" do
+          expect(Util.filter(obj, :method, {}, [:key1, :key2], [ :check, :next ]) { false }).to eql []
+        end
+        it "returns all values when filter is open" do
+          expect(Util.filter(obj, :method, {}, [:key1, :key2], [ :check, :next ]) { true }).to eql ['a']
+        end
+      end
+
+      context "when 2+2+1 is returned, 3 keys" do
+        let(:obj) {
+          Class.new do
+            def method(opts={})
+              if !opts[:next]
+                build(['a', 'b'], 'abcd')
+              elsif opts[:next] == 'abcd'
+                build(['c', 'd'], 'efgh')
+              else
+                build(['e'], nil)
+              end
+            end
+            private
+            def build(value, check)
+              OpenStruct.new(
+                key1: OpenStruct.new(
+                  key2: OpenStruct.new(
+                    key3: value,
+                  ),
+                ),
+                check: check,
+              )
+            end
+          end.new
+        }
+
+        it "returns all values when filter is open" do
+          expect(
+            Util.filter(obj, :method, {}, %w(key1 key2 key3), [ :check, :next ]) { true }
+          ).to eql ['a', 'b', 'c', 'd', 'e']
+        end
+        it "returns no values when filter is closed" do
+          expect(
+            Util.filter(obj, :method, {}, %w(key1 key2 key3), [ :check, :next ]) { false }
+          ).to eql []
+        end
+      end
+    end
   end
 end
